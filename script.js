7e00ba0653780824643ca7808c249021dee691bc
Ethan Jin <ejin05@csu.fullerton.edu>
2026-05-15 21:05:01 -0700

    Created new button that would generate a new grid

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const divContainer = document.querySelector("div.container");
-const body = document.querySelector("body");
-const divBtn = document.createElement("div");
-const gridBtn = document.createElement("button");
+const gridBtn = document.querySelector("button");
 
-body.appendChild(divBtn);
+gridBtn.addEventListener("click", (e) => {
+    let userPrompt = prompt("Please enter grid size:");
+    let num = parseInt(userPrompt);
+    let gridSize = num * num;
+    let basisValue = 100 / num;
 
-const userPrompt = prompt("Please enter grid size:");
-const gridSize = parseInt(userPrompt) * parseInt(userPrompt);
-let basisValue = 100 / parseInt(userPrompt);
+    /*check if user inputs crazy stuff*/
+    while (num <= 0 || num > 100 || isNaN(num)) {
+        userPrompt = prompt("Please enter grid size:");
+        num = parseInt(userPrompt);
+        gridSize = num * num;
+        basisValue = 100 / num;
+    }
 
-for (let i = 1; i <= gridSize; i++) {
-    const div = document.createElement("div");
-    div.classList.add("box");
-    divContainer.appendChild(div);
-    div.style.flex = "0 0 " + basisValue + "%";
-    div.addEventListener("mouseover", (e) => {
-        div.style.backgroundColor = "black";
-    });
-}
+    divContainer.replaceChildren();
+
+    /*generate thy grid*/
+    for (let i = 1; i <= gridSize; i++) {
+        const div = document.createElement("div");
+        div.classList.add("box");
+        divContainer.appendChild(div);
+        div.style.flex = "0 0 " + basisValue + "%";
+        div.addEventListener("mouseover", (e) => {
+            div.style.backgroundColor = "black";
+        });
+    }   
+});
+
+
+
+
